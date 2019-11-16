b419f53ac35f662db8f4ed8c7b55e3a85a0cd633
crazymrwill <56270882+crazymrwill@users.noreply.github.com>
2019-11-16 15:40:08 -0500

    new gravity prototype project made by will

---
+++ gravity/Assets/RotateSelf.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+
+    public float rotateSpeed = 0f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ gravity/Assets/WASD_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Controller : MonoBehaviour
+{
+    public float speed = 0.2f;
+    public KeyCode upButton;
+    public KeyCode downButton;
+    public KeyCode leftButton;
+    public KeyCode rightButton;
+    
+    
+    void Update()
+    {
+        if (Input.GetKey(upButton))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(downButton))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(leftButton))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        }
+        if (Input.GetKey(rightButton))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        }
+    }
+}

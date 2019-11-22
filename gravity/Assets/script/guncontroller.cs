94a9694d287a51dc8fd0d51b8956d2564270a790
crazymrwill <56270882+crazymrwill@users.noreply.github.com>
2019-11-22 10:20:58 -0500

    latest change 11.20

---
+++ gravity/Assets/script/guncontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guncontroller : MonoBehaviour
+{
+    public bool isFiring;
+
+    public bulletcontroller bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                Instantiate(bullet, firePoint.position, firePoint.rotation);
+            }
+        }
+    }
+}
